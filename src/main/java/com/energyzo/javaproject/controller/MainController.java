af64965ae61f0e3e2e45e3e151fb47b13d40c5fb
ownyeon <psbbsp303@gmail.com>
2024-12-12 18:48:05 +0900

    서연 결제관련 업로드

--- src/main/java/com/energyzo/javaproject/controller/MainController.java
+++ src/main/java/com/energyzo/javaproject/controller/MainController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.energyzo.javaproject.model.vo.AgentVO;
 import com.energyzo.javaproject.model.vo.EstSearchDTO;
 import com.energyzo.javaproject.model.vo.EstateVO;
+import com.energyzo.javaproject.service.MypageService;
 import com.energyzo.javaproject.service.SearchService;
 
 
@@ -19,6 +21,11 @@ public class MainController {
 
 	@Autowired
 	SearchService service;
+	
+	
+	@Autowired
+	private MypageService mypageService;
+	
 	
 	@RequestMapping("/main.do")
 	public String startMain(Model model) {
@@ -51,5 +58,42 @@ public class MainController {
 	    // 로그아웃 후 메인 페이지로 리다이렉트
 	    return "redirect:/main.do";
 	}
+	
+	@RequestMapping("remain.do")
+	public String main(HttpSession session) {
+	    session.invalidate();
+		return "main/main";
 
+	}
+
+	
+
+	//판매자 전환시 따지는 함수
+	//1일 경우 대기
+	//2일 경우 판매자 승인
+	@RequestMapping("agentchange.do")
+	public String agentchange(HttpSession session,  Model model) {
+		 String user_id = (String) session.getAttribute("showNewLoginPage");
+
+		 
+		    AgentVO agent = mypageService.getAgent(user_id);
+		    //System.out.println(agent.toString());
+		    
+		    if (agent == null) {
+		        System.out.println("Agent 정보를 찾을 수 없습니다. 판매자 신청 페이지로 이동합니다.");
+		        return "mypage/sellerapplication";
+		    }
+		    
+		    int reg_state  = agent.getAgent_reg_state();
+		    //System.out.println(reg_state);
+		
+		    model.addAttribute("agent", agent);
+		    
+		    if(reg_state == 2) {
+		    	return "mypage/seller";
+		    }else {
+		    	System.out.println("판매자 승인 대기중입니다.");
+		    	return "mypage/sellerapplication2";
+		    }
+	}
 }
